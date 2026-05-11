1b588777baa103f41f54a1eb9ae093aeccd9729e
root <root@zhora1047.startdedicated.com>
2026-05-11 17:36:40 +0000

    Update: Mon May 11 05:36:40 PM UTC 2026

---
+++ kodexplorer/data/system/system_member.php
@@ -0,0 +1,19 @@
+<?php exit;?>{
+    "1": {
+        "name": "admin",
+        "path": "admin",
+        "password": "d73833acccbf342cabcfebc3c02f7df7",
+        "userID": "1",
+        "role": "1",
+        "config": {
+            "sizeMax": "0",
+            "sizeUse": 1024
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "createTime": 1773531743,
+        "status": 1,
+        "lastLogin": 1777021934
+    }
+}
